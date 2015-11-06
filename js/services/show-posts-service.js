7011c77415ff2b334afc467d2ed7f0e6b5323d8b
Juan Sicilia <siciliajuan@gmail.com>
2015-11-06 15:27:18 +0100

    adding styles

--- js/services/show-posts-service.js
+++ js/services/show-posts-service.js
@@ -6,10 +6,12 @@
 			all: function(){
 				var posts = [];
 				var post1 = {
+					id: 1,
 					title: 'Título 1',
 					content: 'Primer post'
 				};
 				var post2 = {
+					id: 2,
 					title: 'Título 2',
 					content: 'Segundo post'
 				};
